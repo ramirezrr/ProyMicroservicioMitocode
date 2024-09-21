3f31f049d707928d7e824a343bc82e0c21dafdc7
RodrigoR <r4mirez.r@gmail.com>
2024-09-21 00:57:45 -0500

    feat(SB-GestionLicencias-api): Creación de proyecto con las dependencias necesarias.

---
+++ SB-GestionLicencias-api/src/main/java/com/mitocode/licencias/SbGestionLicenciasApiApplication.java
@@ -0,0 +1,13 @@
+package com.mitocode.licencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbGestionLicenciasApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbGestionLicenciasApiApplication.class, args);
+    }
+
+}
---
+++ SB-GestionLicencias-api/src/test/java/com/mitocode/licencias/SbGestionLicenciasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitocode.licencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbGestionLicenciasApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
