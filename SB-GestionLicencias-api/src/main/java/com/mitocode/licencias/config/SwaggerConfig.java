22a568b754335a38a323260e8ca633a2c97f5278
RodrigoR <r4mirez.r@gmail.com>
2024-09-25 20:48:15 -0500

    feat(config-server--SB-GestionLicencias-api) {Sync-aa1} Se creó y configuró el proyecto config server para el manejo de propiedades.
    - Se creo y conf el proyecto config-server
    - Se configuró para que Licencias API actue como cliente del config server.
    - Los bootstraps que están dentro del proyecto eureka y licencias ya no tienen perfil porque este lo manejará config server
    - Se configuró para que config server maneje y gestione el archivo de configuración de Licencias api.
    - Cuando levantes los servicios solo importa el profile q indica en el bootstrap.yml que está amarrada a la dependencia al config server.
    - Ahora application.yml tendrá la conf. de Eureka

--- SB-GestionLicencias-api/src/main/java/com/mitocode/licencias/config/SwaggerConfig.java
+++ SB-GestionLicencias-api/src/main/java/com/mitocode/licencias/config/SwaggerConfig.java
@@ -29,7 +29,7 @@ public class SwaggerConfig {
                                 .name("LICENSE")
                                 .url("LICENSE URL")))
                       .servers(List.of(new Server()
-                            .url("http://localhost:8080/gestor-licencias-api")
+                            .url("http://localhost:9010/gestor-licencias-api")
                             .description("Servidor de desarrollo"))
                       );
     }
